143fc8c809f4d15f87a9e23a241c3f108882c052
Manikandan Arunachalam <manikandanarunachalam913@gmail.com>
2023-06-05 12:23:00 +0530

    first commit

---
+++ assets/mk.js
@@ -0,0 +1,15 @@
+<!-- ("Hi friends This is Manikandan Portfolio"); //-->
+//data types
+
+//Numbers
+
+//string
+
+
+//arrays
+
+//object
+
+//variable declaration
+var h1="Maniknadan U K I";
+console.log=(h1);
